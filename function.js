a58b150d39c61590313ae938100a41ea6d7a7a3c
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-09-11 18:23:59 +0530

    sec commit

--- function.js
+++ function.js
@@ -1,17 +1,28 @@
 // 1.Declaretion of function 
 
-function sum() {
-    var a = 10; b = 5;
-    var total = a + b;
+// function sum() {
+//     var a = 10; b = 5;
+//     var total = a + b;
+//     console.log(total);
+// }
+
+// function multiple() {
+//     var a = 10; b = 5;
+//     var total = a * b;
+//     console.log(total);
+// }
+// // 2.call this function 
+// sum();
+// multiple();
+ 
+
+// function paramenter vs function argument //
+
+function sum(a,b){
+    var total = a+b;
     console.log(total);
 }
 
-function multiple() {
-    var a = 10; b = 5;
-    var total = a * b;
-    console.log(total);
-}
-// 2.call this function 
-sum();
-multiple();
-
+// calling function 
+sum(20,10)
+sum(23,2)
