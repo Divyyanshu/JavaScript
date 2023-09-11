a11d9b44e3f493711467733a64c33697bd3426bd
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-09-11 19:05:12 +0530

    chnge

--- function.js
+++ function.js
@@ -1,50 +1,50 @@
-1.Declaretion of function 
+// 1.Declaretion of function 
 
-function sum() {
-    var a = 10; b = 5;
-    var total = a + b;
-    console.log(total);
-}
+// function sum() {
+//     var a = 10; b = 5;
+//     var total = a + b;
+//     console.log(total);
+// }
 
-function multiple() {
-    var a = 10; b = 5;
-    var total = a * b;
-    console.log(total);
-}
-// 2.call this function 
-sum();
-multiple();
+// function multiple() {
+//     var a = 10; b = 5;
+//     var total = a * b;
+//     console.log(total);
+// }
+// // 2.call this function 
+// sum();
+// multiple();
  
 
-3.function paramenter vs function argument //
+// 3.function paramenter vs function argument //
 
-function sum(a,b){
-    var total = a+b;
-    console.log(total);
-}
+// function sum(a,b){
+//     var total = a+b;
+//     console.log(total);
+// }
 
-// calling function 
-sum(20,10)
-sum(23,2)
+// // calling function 
+// sum(20,10)
+// sum(23,2)
 
-4. function expression + return key
+// 4. function expression + return key
 
-function sum(a,b){
-        return total = a+b;
-    }
-    var funExp = sum (10,55);
-    console.log("sum of two no is " + funExp);
+// function sum(a,b){
+//         return total = a+b;
+//     }
+//     var funExp = sum (10,55);
+//     console.log("sum of two no is " + funExp);
 
-5. function anonymous 
+// 5. function anonymous 
 
-var funExp = function(a,b){
-    return total = a+b;
-}
-calling function
-var sum = funExp(55,5);
-var sum1 = funExp(21,40)
-console.log(sum >= sum1);
-console.log("sum of two no is " + sum*sum1);
+// var funExp = function(a,b){
+//     return total = a+b;
+// }
+// calling function
+// var sum = funExp(55,5);
+// var sum1 = funExp(21,40)
+// console.log(sum >= sum1);
+// console.log("sum of two no is " + sum*sum1);
 
 
 // --------------------------------------------------------------------
