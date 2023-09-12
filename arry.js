4a188bba6683e1ce27a8d29137fdc0445fdb596f
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-09-12 19:15:11 +0530

    fouth commit

--- arry.js
+++ arry.js
@@ -8,3 +8,45 @@
 // console.log(num[num.length-1]);
 // console.log(num.length)
 
+// let num = ["jai",5,true,"dhwani","shyam","divyanshu"];
+// using for loop to fatch all data ine by one 
+
+// for(let i=0;i<num.length;i++){
+//       console.log(num[i])
+// } //FOR LOOP 
+
+// let i=0;
+// while(i<num.length){
+//     console.log(num[i]);
+//     i++;
+// } USING WHILE LOOP 
+
+// THEN 🤞🤞🤞❤️(FOR IN LOOP) & (FOR OF LOOP) TO FATCH THE DATA ONE BY ONE AND EASILY THIS LOOP IS EC6 //
+
+// let num = ["jai",5,true,"dhwani","shyam","divyanshu","tailor"];
+  
+
+// //1.😊 FOR IN LOOP = ITS GIVE THE ALL INDEX VALUE OF THE ELEMENTS.
+//  for (let elements in num){
+//     console.log(elements);
+//  }
+
+// //2.😊 FOR OF LOOP =  ITS GIVE THE ELEMENTS VALUE OF THE ARRAY.
+
+// for (let elements of num){
+//     console.log(elements);
+// }
+
+
+//use most important and most using loop (forEach) its make a one fuction then cALL The function 
+
+let num = ["jai","dhwani","shyam","divyanshu","tailor"];
+
+// num.forEach(function(element,index,array){
+// console.log(element + " index: " + index + " " + array)
+// })
+
+// flat arrow function in es6
+num.forEach((element,index,array)=>{
+    console.log(element + " index: " + index + " " + array)
+    });
